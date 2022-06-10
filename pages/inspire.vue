2c10d299f4e1235941433270888cce1b0eedb702
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-06-10 15:52:08 -0700

    Initial personalized text.

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,10 +7,10 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;We are a way for the universe to know itself.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash; Carl Sagan</em>
           </small>
         </footer>
       </blockquote>
