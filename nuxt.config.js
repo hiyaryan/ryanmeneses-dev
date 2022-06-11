430458bb1ad20690d9cb764ed6115d0f30d3c2c9
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-06-10 17:58:26 -0700

    Update page header.

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - ryanmeneses',
-    title: 'ryanmeneses',
+    titleTemplate: '%s',
+    title: 'Ryan Meneses',
     htmlAttrs: {
       lang: 'en'
     },
